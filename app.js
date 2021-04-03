b575e7f928891ac150ff611c56743ec485df8f93
kaithill <k.m.hiller@gmail.com>
2021-04-03 11:42:02 -0400

    connect MongoDB

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
